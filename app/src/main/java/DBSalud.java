762a6f0293cf9581dbe9cb20564dde8a559c5258
Miguel García <mgfr.garcia.garcia@gmail.com>
2022-02-08 13:58:28 +0100

    base de datos?

---
+++ app/src/main/java/DBSalud.java
@@ -0,0 +1,29 @@
+public class DBSalud {
+
+    //TABLA USUARIO
+
+    public static final String USUARIO_TABLE_USUARIO = "USUARIO";
+    public static final String USUARIO_COL_ID_USUARIO = "ID_USUARIO";
+    public static final String USUARIO_COL_NOMBRE = "NOMBRE";
+    public static final String USUARIO_COL_CORREO = "CORREO";
+    public static final String USUARIO_COL_CONTRA = "CONTRA";
+
+
+    public static final String ANIMAL_TABLE_ANIMAL = "ANIMAL";
+    public static final String ANIMAL_COL_ID_ANIMAL = "ID_ANIMAL";
+    public static final String ANIMAL_COL_ID_USUARIO = "ID_USUARIO";
+    public static final String ANIMAL_COL_NOMBRE = "NOMBRE";
+    public static final String ANIMAL_COL_ANYO = "ANYO";
+    public static final String ANIMAL_COL_MES = "MES";
+    public static final String ANIMAL_COL_RAZA = "RAZA";
+    public static final String ANIMAL_COL_SEXO = "SEXO";
+
+    public static final String SEGUIMIENTO_TABLE_SEGUIMIENTO = "SEGUIMIENTO";
+    public static final String SEGUIMIENTO_COL_ID_SEGUIMIENTO = "ID_SEGUIMIENTO";
+    public static final String SEGUIMIENTO_COL_ID_ANIMAL = "ID_ANIMAL";
+    public static final String SEGUIMIENTO_COL_TIPO = "TIPO";
+    public static final String SEGUIMIENTO_COL_PESO = "PESO";
+    public static final String SEGUIMIENTO_COL_DESCRIPCION = "DESCRIPCION";
+    public static final String SEGUIMIENTO_COL_FECHA = "FECHA";
+
+}
---
+++ app/src/main/java/SALUDSqlHelper.java
@@ -0,0 +1,82 @@
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SALUDSqlHelper extends SQLiteOpenHelper {
+
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_FILE_NAME = "DB_SALUD.db";
+
+    private SQLiteDatabase db;
+    private static SALUDSqlHelper SALUDSqlHelper;
+
+    String sqlCreateUSUARIO = "CREATE TABLE " + DBSalud.USUARIO_TABLE_USUARIO + "("
+            + DBSalud.USUARIO_COL_ID_USUARIO + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + DBSalud.USUARIO_COL_NOMBRE + " TEXT,"
+            + DBSalud.USUARIO_COL_CORREO + " TEXT,"
+            + DBSalud.USUARIO_COL_CONTRA + " TEXT);";
+
+    String sqlCreateANIMAL = "CREATE TABLE " + DBSalud.ANIMAL_TABLE_ANIMAL
+            + "(" + DBSalud.ANIMAL_COL_ID_ANIMAL + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + DBSalud.ANIMAL_COL_ID_USUARIO + " INTEGER,"
+            + DBSalud.ANIMAL_COL_NOMBRE + " TEXT,"
+            + DBSalud.ANIMAL_COL_ANYO + " INTEGER,"
+            + DBSalud.ANIMAL_COL_MES + " INTEGER,"
+            + DBSalud.ANIMAL_COL_RAZA + " TEXT,"
+            + DBSalud.ANIMAL_COL_SEXO + " TEXT,"
+            + " FOREIGN KEY(" + DBSalud.ANIMAL_COL_ID_USUARIO
+            + ") REFERENCES " + DBSalud.USUARIO_TABLE_USUARIO
+            + "(" + DBSalud.ANIMAL_COL_ID_USUARIO + "));";
+
+    String sqlCreateSEGUIMIENTO = "CREATE TABLE " + DBSalud.SEGUIMIENTO_TABLE_SEGUIMIENTO
+            + "(" + DBSalud.SEGUIMIENTO_COL_ID_SEGUIMIENTO + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + DBSalud.SEGUIMIENTO_COL_ID_ANIMAL + " INTEGER,"
+            + DBSalud.SEGUIMIENTO_COL_TIPO + " TEXT,"
+            + DBSalud.SEGUIMIENTO_COL_PESO + " INTEGER,"
+            + DBSalud.SEGUIMIENTO_COL_DESCRIPCION + " TEXT,"
+            + DBSalud.ANIMAL_COL_SEXO + " TEXT,"
+            + " FOREIGN KEY(" + DBSalud.SEGUIMIENTO_COL_ID_ANIMAL
+            + ") REFERENCES " + DBSalud.ANIMAL_TABLE_ANIMAL
+            + "(" + DBSalud.SEGUIMIENTO_COL_ID_ANIMAL + "));";
+
+    private SALUDSqlHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(sqlCreateUSUARIO);
+        sqLiteDatabase.execSQL(sqlCreateANIMAL);
+        sqLiteDatabase.execSQL(sqlCreateSEGUIMIENTO);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DBSalud.USUARIO_TABLE_USUARIO);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DBSalud.ANIMAL_TABLE_ANIMAL);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DBSalud.SEGUIMIENTO_TABLE_SEGUIMIENTO);
+        sqLiteDatabase.execSQL(sqlCreateUSUARIO);
+        sqLiteDatabase.execSQL(sqlCreateANIMAL);
+        sqLiteDatabase.execSQL(sqlCreateSEGUIMIENTO);
+    }
+
+    public static synchronized SALUDSqlHelper getInstance(Context context) {
+        if (SALUDSqlHelper == null) {
+            SALUDSqlHelper = new SALUDSqlHelper(context, DATABASE_FILE_NAME, null, DATABASE_VERSION);
+        }
+        return SALUDSqlHelper;
+    }
+
+    public synchronized SQLiteDatabase open() throws SQLException {
+        return SALUDSqlHelper.getWritableDatabase();
+    }
+
+    public synchronized void close() {
+        db.close();
+    }
+
+}
